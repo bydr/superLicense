e9adfd24359785cb83775f2772a6d9c4c79c5524
Daniil <zeveres88@gmail.com>
2020-08-25 08:52:36 +0300

    added favicon

--- app/head.php
+++ app/head.php
@@ -14,12 +14,16 @@
 
     <!-- Template Basic Images Start -->
     <meta property="og:image" content="path/to/image.jpg">
-    <link rel="icon" href="img/favicon/favicon.ico">
-    <link rel="apple-touch-icon" sizes="180x180" href="img/favicon/apple-touch-icon-180x180.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+    <link rel="manifest" href="favicon/site.webmanifest">
+    <link rel="mask-icon" href="favicon/safari-pinned-tab.svg" color="#01939a">
+    <meta name="msapplication-TileColor" content="#ffffff">
     <!-- Template Basic Images End -->
 
     <!-- Custom Browsers Color Start -->
-    <meta name="theme-color" content="#000">
+    <meta name="theme-color" content="#01939a">
     <!-- Custom Browsers Color End -->
 
     <link rel="stylesheet" href="css/main.min.css">
